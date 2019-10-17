01b161bf0c8b0c8ae465c75080f2f2b80c5ce550
lailailee <18292889055@163.com>
2019-10-17 23:50:43 +0800

    deemo08&04.Plugin-使打包更加便捷&05.Entry与Output的基本配置

---
+++ deemo/deemo08-entry-output/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/index.js',
+    sub: './src/index.js'
+  },
+  module: {
+    rules: [{
+      test: /\.(jpg|png|gif)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          name: '[name]_[hash].[ext]',
+          outputPath: 'images/',
+          limit: 10240
+        }
+      }
+    }, {
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 2,
+          }
+        },
+        'sass-loader',
+        'postcss-loader'
+      ]
+    },
+    {
+      test: /\.(eot|ttf|svg|woff)$/,
+      use: {
+        loader: 'file-loader',
+      }
+    }]
+  },
+  plugins: [new HtmlWebpackPlugin(
+    {
+      tempalte: 'src/index.html',
+    }
+  ), new CleanWebpackPlugin()],
+  output: {
+    publicPath: 'http://cdn.com.cn',
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
